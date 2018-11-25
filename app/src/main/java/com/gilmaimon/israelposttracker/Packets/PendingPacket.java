7552ab64fe5e90ca4600e51bd8badcdcce6d3bff
Gil Maimon <gilm2121@gmail.com>
2018-11-25 02:28:29 +0200

    added lastNotice field for PendingPacket

--- app/src/main/java/com/gilmaimon/israelposttracker/Packets/PendingPacket.java
+++ app/src/main/java/com/gilmaimon/israelposttracker/Packets/PendingPacket.java
@@ -1,13 +1,21 @@
 package com.gilmaimon.israelposttracker.Packets;
 
+import java.util.Date;
+
 public class PendingPacket extends Packet {
     private int branchId;
     private String branchPacketId;
+    private Date lastNotice;
 
-    public PendingPacket(String postId, int branchId, String branchPacketId) {
+    public PendingPacket(String postId, int branchId, String branchPacketId, Date lastNotice) {
         super(postId);
         this.branchId = branchId;
         this.branchPacketId = branchPacketId;
+        this.lastNotice = lastNotice;
+    }
+
+    public void setLastNotice(Date date) {
+        this.lastNotice = date;
     }
 
     public int getBranchId() {
@@ -16,5 +24,9 @@ public class PendingPacket extends Packet {
 
     public String getBranchPacketId() {
         return branchPacketId;
+    }
+
+    public Date getLastNotice() {
+        return this.lastNotice;
     }
 }
